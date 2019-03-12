26c70f91b5ff7c280de6c31a666a0e421fcc5f5d
WorkUser <136903973@qq.com>
2019-03-12 16:15:11 +0800

    Job-2

---
+++ Proxy-Pattern-20190311/src/main/java/com/btlimt/vip/App.java
@@ -0,0 +1,13 @@
+package com.btlimt.vip;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
